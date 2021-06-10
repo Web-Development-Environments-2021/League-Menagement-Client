048af5dbf0e59d1a1dcd631e385bc66bdf00e1f5
shimon <shimon.on5@gmail.com>
2021-06-11 00:17:25 +0300

    past games (no events)

--- src/routes.js
+++ src/routes.js
@@ -2,42 +2,51 @@ import Main from "./pages/MainPage";
 import NotFound from "./pages/NotFoundPage";
 import FavoriteGames from "./components/FavoriteGames";
 
-const routes = [
-  {
-    path: "/",
-    name: "main",
-    component: Main
-  },
-  {
-    path: "/register",
-    name: "register",
-    component: () => import("./pages/RegisterPage")
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: () => import("./pages/LoginPage")
-  },
-  {
-    path: "/logout",
-    name: "logout",
-    component: () => import("./pages/LoginPage")
-  },
-  {
-    path: "/search",
-    name: "search",
-    component: () => import("./pages/SearchPage")
-  },
-  {
-    path: "/favorite",
-    name: "favorite",
-    component: FavoriteGames
-  },
-  {
-    path: "*",
-    name: "notFound",
-    component: NotFound
-  }
+const routes = [{
+        path: "/",
+        name: "main",
+        component: Main
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: () =>
+            import ("./pages/RegisterPage")
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: () =>
+            import ("./pages/LoginPage")
+    },
+    {
+        path: "/logout",
+        name: "logout",
+        component: () =>
+            import ("./pages/LoginPage")
+    },
+    {
+        path: "/search",
+        name: "search",
+        component: () =>
+            import ("./pages/SearchPage")
+    },
+    {
+        path: "/currentStage",
+        name: "currentStage",
+        component: () =>
+            import ("./pages/currentStagePage")
+    },
+    {
+        path: "/favorite",
+        name: "favorite",
+        component: FavoriteGames
+    },
+    {
+        path: "*",
+        name: "notFound",
+        component: NotFound
+    }
 ];
 
 export default routes;
