bdffd841e0502fb4f33cfe0844f014baf22c6328
shimon <shimon.on5@gmail.com>
2021-07-06 21:21:09 +0300

    //

---
+++ src/pages/AdminPage.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="admin-page">
+        <button @click="showForm"> Insert New Game</button>
+        <b-modal ref="gameForme" id="bv-modal-example" @hide="cleanForm" hide-footer>
+        <select v-model="selectedAway">
+        <option disabled value="">Please select Away Team</option>
+        <option v-for="team in teams" :key="team.team_name">
+            {{team.team_name}}
+            <template slot="option">
+                            <img :src="`${team.logo_path}`" class="card-img-top" style="height: 200px; width: auto"/>
+            </template>
+        </option>      
+        </select>
+        <span>Selected: {{ selectedAway }}</span>
+
+        <select v-model="selectedHome">
+        <option disabled value="">Please select Home Team</option>
+        <option v-for="team in teams" :key="team.team_name">
+            {{team.team_name}}
+            <template slot="option">
+                            <img :src="`${team.logo_path}`" class="card-img-top" style="height: 200px; width: auto"/>
+            </template>
+        </option>      
+        </select>
+        <span>Selected: {{ selectedHome }}</span>
+        </b-modal>
+    </div>
+</template>
+<script>
+import Datepicker from 'vue3-datepicker'
+import { ref } from 'vue'
+const picked = ref(new Date());
+export default{
+    name: "admin-page",
+    data: function(){
+        return{
+        selectedAway: '',
+        selectedHome: '',
+        teams:[],
+
+        }
+    },
+    methods: {
+            async showForm(){
+                this.$refs["gameForme"].show();
+                let urlPath = `http://localhost:3000/teams/getAllTeamsByCountry/320`;
+                const teams = await this.axios.get(
+                        urlPath,{withCredentials: true});
+                console.log(teams)
+                this.teams = teams.data;
+            },
+            cleanForm(){
+                this.selectedAway = '';
+                this.selectedHome = '';
+            }
+
+        },
+}
+</script>
